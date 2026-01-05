803bf9ab73749cf673eb6f0b23f824f9e9df6b27
julianpoyourow <julian@poyourow.com>
2026-01-05 23:24:45 +0000

    fix: update esbuild with jsdom externalized

--- packages/queue-worker/src/main.ts
+++ packages/queue-worker/src/main.ts
@@ -1,15 +1,35 @@
 import "./sentry-init.js";
+import express from "express";
+import client from "prom-client";
+import { promisify } from "util";
 
 import { jobQueueWorker } from "@recipesage/util/server/general";
 
 jobQueueWorker.run();
 
+const app = express();
+const port = parseInt(process.env.PORT || "3000");
+
+app.get("/metrics", async (_req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.get("/health", (_req, res) => {
+  if (jobQueueWorker.closing) res.status(500).send("Queue closing");
+  else res.status(200).send("Healthy");
+});
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
 const close = async () => {
   console.log("CLOSING WORKER DUE TO SIGNAL");
 
-  await jobQueueWorker.close();
+  await Promise.allSettled([jobQueueWorker.close(), promisify(server.close)()]);
 
-  console.log("EXITED CLEANLY");
+  console.log("EXITED");
 
   process.exit(0);
 };
