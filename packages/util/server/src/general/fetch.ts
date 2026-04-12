25b091585e848c9a745849c011f62643c558bd33
julianpoyourow <julian@poyourow.com>
2026-04-12 03:18:48 +0000

    chore: update fetch headers

--- packages/util/server/src/general/fetch.ts
+++ packages/util/server/src/general/fetch.ts
@@ -39,7 +39,18 @@ export const fetchURL = (
     ...options?.requestConfig,
     headers: {
       "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Cache-Control": "no-cache",
+      Pragma: "no-cache",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-User": "?1",
+      "Upgrade-Insecure-Requests": "1",
       ...options?.requestConfig?.headers,
     },
   };
