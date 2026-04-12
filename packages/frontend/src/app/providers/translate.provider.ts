73f3646713cd91e26431c6b1aee32506f5fbf5dc
julianpoyourow <julian@poyourow.com>
2026-04-12 06:10:15 +0000

    fix: update translate provider for ngx-translate v17 API

--- packages/frontend/src/app/providers/translate.provider.ts
+++ packages/frontend/src/app/providers/translate.provider.ts
@@ -1,21 +1,15 @@
-import { HttpClient } from "@angular/common/http";
-import { provideTranslateService, TranslateLoader } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { provideTranslateService } from "@ngx-translate/core";
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
 import { SupportedLanguages } from "@recipesage/util/shared";
 
-export function createTranslateLoader(http: HttpClient) {
-  const prefix = "assets/i18n/";
-  const suffix = `.json?version=${(window as any).version}`;
-  return new TranslateHttpLoader(http, prefix, suffix);
-}
-
 export function provideTranslate() {
-  return provideTranslateService({
-    loader: {
-      provide: TranslateLoader,
-      useFactory: createTranslateLoader,
-      deps: [HttpClient],
-    },
-    defaultLanguage: SupportedLanguages.EN_US,
-  });
+  return [
+    ...provideTranslateService({
+      fallbackLang: SupportedLanguages.EN_US,
+    }),
+    ...provideTranslateHttpLoader({
+      prefix: "assets/i18n/",
+      suffix: `.json?version=${(window as any).version}`,
+    }),
+  ];
 }
